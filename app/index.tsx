e7cce7daff9c04d644312d729d111415660a5508
Salim Hasan <salimhasan.co.in@gmail.com>
2025-08-03 14:57:36 +0530

    Add tailwind native to the code and setup the styling components
    Add tailwind native to the code and setup the styling components

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-red-100">
+      <Text className="text-2xl font-medium text-black">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
